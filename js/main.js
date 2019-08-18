182e70595aef661ea854334f1756970a2fa828ad
chatchai590112416002 <chatchai.kae@bru.ac.th>
2019-08-18 12:20:43 +0700

    init

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function getData(){
+       value = document.getElementById('in1').valie;
+        showData(value);
+
+	};
+
+     function showData(msg){
+     console.log(msg);
+         document.getElementById('lb1').innerHTML=msg;
+
+
+
+     };
